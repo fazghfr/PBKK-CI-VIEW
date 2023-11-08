ac90f7694b23368b5bd8d75a2e84e177df12bb92
fauzan <fauzanalghifari963@gmail.com>
2023-11-08 18:56:01 +0700

    final commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/landing', 'Landing::index');
+$routes->get('/about', 'About::index');
---
+++ app/Views/about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+    <style>
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+            font-size: 24px;
+            color: #333;
+            text-align: center;
+            margin-top: 100px;
+        }
+        h1 {
+            font-size: 72px;
+            color: #ff6600;
+            margin-bottom: 50px;
+        }
+    </style>
+</head>
+<body>
+    <h1>About Page</h1>
+    <p>Anda berada pada Halaman About!</p>
+</body>
+</html>
